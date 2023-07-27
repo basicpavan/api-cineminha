ac8367b2244ce39ce95b3058064d35c3da3041cd
Gustavo <gustavopavan993@gmail.com>
2023-07-27 13:38:26 -0300

    <3

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('u fond us')
-});
 
-app.get('/user', (req, res) => {
-    res.send('esta dentro do usuário')
-});
 
 module.exports = app;

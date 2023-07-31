c846384751ff2137f7ae2e9f9f7aad9f6097b171
ovinhovini <122178083+ovinhovini@users.noreply.github.com>
2023-07-31 16:28:57 -0300

    Update server.js
    ;

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const http = require("http");
-const app = require("./app")
+const app = require("./app");
 const port = process.env.PORT || 3001;
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-
-
-
-
-
-
-server.listen(port)
+server.listen(port);
